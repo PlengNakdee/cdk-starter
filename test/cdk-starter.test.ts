2fc148120ee571d94e79bccbd88a84f0c73efc20
Pleng Nakdee <plengwn@gmail.com>
2023-02-20 17:47:25 +0700

    remove test

--- test/cdk-starter.test.ts
+++
@@ -1,9 +0,0 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkStarter from '../lib/cdk-starter-stack';
-
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-starter-stack.ts
-test('SQS Queue Created', () => {
-    //
-});
