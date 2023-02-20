8420dc4674d85e535907d9e409f16a92b5b9ce21
Pleng Nakdee <plengwn@gmail.com>
2023-02-20 17:36:53 +0700

    set up

--- test/cdk-starter.test.ts
+++ test/cdk-starter.test.ts
@@ -5,13 +5,5 @@
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-starter-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkStarter.CdkStarterStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    //
 });
